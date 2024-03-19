0f3c12f7dd124b54c743a2a9222514b826482731
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-19 14:37:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+//function halfFryEgg() {
+// let cocked = 1 + 1.5 + 2;
+// console.log(cocked)
+// }
+// halfFryEgg() //Invoking the function
+// function halfFryEgg(){
+// let cooked = 1 + 1.5 + 2;
+// console.log(cooked);
+// return cooked
+// }
+// function halfFryEgg():string{
+// const cooked = "Hello"+"Alisha"
+// console.log(cooked);
+// return cooked
+// }
+// function myTeachers(teacher1:string,teacher2:string,teacher3:string,teacher4:string){
+// return teacher1 + teacher2 + teacher3 + teacher4
+// }
+// let response: string = myTeachers("Asharib","Ubaid","Naeem","Hafsa")
+//console.log(response);
+function checkNumber(num) {
+    if (num > 2) {
+        console.log("The number is six.");
+    }
+    else if (num < 2) {
+        console.log("The number is -four.");
+    }
+    else {
+        console.log("The number is two.");
+    }
+}
+// Example usage
+checkNumber(6); // Output: The number is six.
+checkNumber(-4); // Output: The number is -four.
+checkNumber(2); // Output: The number is two.
